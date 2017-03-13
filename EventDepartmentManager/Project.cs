b770a386ad637646919d147bc36cb24635b3b57c
Natalya <Natalya@NATALYA-PC>
2017-03-13 13:15:20 +0300

    Entity structure

---
+++ EventDepartmentManager/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDepartmentManager
+{
+    class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime EndDate { get; set; }
+        public Client Client { get; set; }
+        public List<Employee> Team { get; set; }
+    }
+}
